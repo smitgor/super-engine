887fa9246bed542fa9120437034fcaf466291e2b
Partha <iamparthaonline@gmail.com>
2024-08-01 21:24:18 +0530

    inventory page - wip

--- imports/ui/pages/Inventory/index.jsx
+++ imports/ui/pages/Inventory/index.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
 
+const items = [
+    'beetroot.avif',
+    'capsicum.avif',
+    'carrot.avif',
+    'cauliflower.avif',
+    'cucumber.avif',
+    'garlic.avif',
+    'ginger.avif',
+    'green-chilli.avif',
+    'ladyfinger.avif',
+    'lemon.avif',
+    'mushroom.avif',
+    'onion.avif',
+    'potato.avif',
+    'tomato.avi',
+];
+
 const Orders = () => {
     return (
-        <div className="heading">
-            {_t('Introduction')} -- {_t('hello')}
+        <div className="inventory-page-container">
+            <div className="heading">{_t('Inventory')}</div>
+            <ul>
+                {items.map((item, i) => (
+                    <li key={item}>
+                        <div className="inventory-meta-container">
+                            <div>
+                                <img src={`items/${item}`} alt="" />
+                            </div>
+                            <div>
+                                <h2>
+                                    {item.substring(0, item.indexOf('.avif'))}
+                                </h2>
+                                {i % 3 === 0 ? (
+                                    <span className="available">
+                                        Available: 20
+                                    </span>
+                                ) : (
+                                    <span className="unavailable">
+                                        Out of Stock
+                                    </span>
+                                )}
+                            </div>
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
